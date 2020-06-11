9d1cda4ec999e217b5ee7b5b5c7785d9ae0e17e5
Krasniqi550 <59025968+Krasniqi550@users.noreply.github.com>
2020-06-11 22:50:47 +0000

    Add files via upload

---
+++ src/contractM/Help.java
@@ -0,0 +1,55 @@
+package contractM;
+
+
+import javafx.scene.Scene;
+
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class Help {
+	
+	public void about() {
+		Tab newtab = new Tab(); 
+		TabPane tbpane = new TabPane();
+		 
+		 Stage helpAboutStage = new Stage();
+	    newtab.setOnSelectionChanged(e-> {if (newtab.isSelected())  
+        { 
+			
+            Tab tab = new Tab("Help" ); 
+      
+    		WebView browser = new WebView();
+
+    		WebEngine webEngine = browser.getEngine();
+
+    		String url = Help.class.getResource("info.html").toExternalForm();
+
+    		System.out.println("Local URL: " + url);
+    		
+    		webEngine.load(url);
+    		
+    		tab.setContent(browser);
+            
+            tbpane.getTabs().add( 
+                    tbpane.getTabs().size() - 1, tab); 
+
+            
+            tbpane.getSelectionModel().select( 
+                    tbpane.getTabs().size() - 2); 
+        } }); 
+	    
+	   tbpane.getTabs().add(newtab);
+
+		Scene sc = new Scene(tbpane, 500, 300);
+
+		helpAboutStage.setTitle("Help - About");
+		helpAboutStage.setScene(sc);
+
+		helpAboutStage.show();
+
+	}
+}
